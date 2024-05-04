c8af16a0e07f8d41f1244e77438ab53e9456bde9
Tembanator <49640058+Tembanator@users.noreply.github.com>
2024-05-04 21:06:22 +0200

    Update WorkItem.jsx

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -10,7 +10,7 @@ const WorkItem = () => {
             </div>
             <div
                 className='absolute z-0 w-full h-full left-0 bottom-0 right-0 top-0 opacity-0 group-hover:opacity-100 duration-300 aspect-square bg-gradient-to-r from-sky-500 to-indigo-500 flex flex-col justify-center items-center gap-4'>
-                <h3 className='font-bold'>React JS Application</h3>
+                <h3 className='font-bold'>Coming Soon</h3>
                 <div className='flex gap-3'>
                     <button
                         className='border-2 border-white px-3 flex justify-center items-center py-1 rounded-md bg-white text-indigo-950 font-semibold'>Demo</button>
